655fc641d60b074a2762a8a2180f85f6794850e8
rezzak ali <rezzak ali>
2023-09-16 08:56:14 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import http from 'http';
+import resolvers from './graphqlResolvers/resolvers.js';
+import typeDefs from './graphqlSchemas/schema.js';
+// app instance
+const app = express();
+const httpServer = http.createServer(app);
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+await server.start();
+app.use('/books', (req, res) => {
+    res.send('Books');
+});
+app.use('/api/v1/', cors(), bodyParser.json(), expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+}));
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Your server is running successfully on http://localhost:4000/`);
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import http from 'http';
+import resolvers from './graphqlResolvers/resolvers.js';
+import typeDefs from './graphqlSchemas/schema.js';
+
+// app instance
+const app = express();
+
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+await server.start();
+
+app.use('/books', (req, res) => {
+  res.send('Books');
+});
+
+app.use(
+  '/api/v1/',
+  cors<cors.CorsRequest>(),
+  bodyParser.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  })
+);
+
+await new Promise<void>((resolve) =>
+  httpServer.listen({ port: 4000 }, resolve)
+);
+console.log(`🚀 Your server is running successfully on http://localhost:4000/`);
